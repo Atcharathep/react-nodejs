d47caef51dacd99bcb24294c284b09f7d0a60fe4
Atcharathep <69839487+Atcharathep@users.noreply.github.com>
2025-04-13 03:04:49 +0700

    Delete server directory

--- server/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/orderRoutes.js
-const express = require('express');
-const orderController = require('../controllers/orderController');
-const router = express.Router();
-
-// เส้นทางการสร้างคำสั่งซื้อ
-router.post('/orders', orderController.createOrder);
-
-module.exports = router;
